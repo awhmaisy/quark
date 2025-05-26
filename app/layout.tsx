4bd554e245a9120ec656e1e732e243b825258e7a
mei higashi <mei@meihigashi.com>
2025-05-25 23:00:46 -0700

    type fxes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+// We're using a local font instead of next/font/google
+// because we want to ensure it works when deployed
+
 export const metadata: Metadata = {
   title: "C:\MAISYVERSE\V3",
   description: "@awhmaisy's personal site",
@@ -19,6 +22,9 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#000000" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
         {children}
