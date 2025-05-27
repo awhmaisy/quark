f5c698256ee0bf8ad61ac50e69ae8a1fa98fbf5d
mei higashi <mei@meihigashi.com>
2025-05-27 09:21:34 -0700

    test type

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#000000" />
         {/* Font preloading for optimal performance */}
         <link rel="preload" href="/fonts/VT323-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/ft-speaker/FTSpeaker-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet" />
@@ -40,7 +41,7 @@ export default function RootLayout({
           
           @font-face {
             font-family: 'FT Speaker';
-            src: local('Arial'), local('Helvetica'), local('sans-serif');
+            src: url('/fonts/ft-speaker/FTSpeaker-Regular.woff2') format('woff2');
             font-weight: normal;
             font-style: normal;
             font-display: swap;
